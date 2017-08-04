1b2ccebc31b15825c1c9638d21705a74947ae3cf
fuzhipeng <1149324777@qq.com>
2017-08-04 14:16:27 +0800

    嵌套滑动抖动问题

--- app/src/main/java/zone/com/zrefreshlayoutdemo/views/ZNestParentLayout.java
+++ app/src/main/java/zone/com/zrefreshlayoutdemo/views/ZNestParentLayout.java
@@ -6,9 +6,7 @@ import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 
@@ -21,14 +19,16 @@ import zone.com.zrefreshlayout.utils.SimpleAnimatorListener;
 
 /**
  * [2017] by Zone
+ * <p>
+ * 嵌套滑动  的onInterceptTouchEvent总是返回true 即使你false了
+ * <p>
+ * 而ontouch却不走
  */
-
 public class ZNestParentLayout extends LinearLayout implements NestedScrollingParent {
     private static final String TAG = "ZNestParentLayout";
     private final NestedScrollingParentHelper mNestedScrollingParentHelper;
     private IFlexScroll mIFlexScroll;
     private IScrollFlex mIScroll = new FlexScroll();//滚动策略
-    private int mTopViewHeightOrg;
 
     public ZNestParentLayout(Context context) {
         this(context, null);
@@ -66,6 +66,7 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
     @Override
     public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
         Log.e(TAG, "onStartNestedScroll");
+        mIScroll.cancel();
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
@@ -76,52 +77,32 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
 
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-        Log.e(TAG, "onNestedPreScroll_getScrollY:"+ mIScroll.getScrollY() );
+//
+//        Log.e(TAG, String.format("scrollto_  dx:%d \tdy:%d \tcanScrollVertically 1:%b \tcanScrollVertically -1:%b " +
+//                        "自己:\tcanScrollVertically 1:%b \tcanScrollVertically -1:%b"
+//                , dx, dy, ViewCompat.canScrollVertically(target, 1), ViewCompat.canScrollVertically(target, -1),
+//                ViewCompat.canScrollVertically(this, 1), ViewCompat.canScrollVertically(this, -1)));
 
-        Log.e(TAG, String.format("scrollto_  dx:%d \tdy:%d \tcanScrollVertically 1:%b \tcanScrollVertically -1:%b " +
-                        "自己:\tcanScrollVertically 1:%b \tcanScrollVertically -1:%b"
-                , dx, dy, ViewCompat.canScrollVertically(target, 1), ViewCompat.canScrollVertically(target, -1),
-                ViewCompat.canScrollVertically(this, 1), ViewCompat.canScrollVertically(this, -1)));
-
-        if (dy < 0) {//下拉
-
-            //父控件消耗的两种情况：
-            //1.下啦 显示头部  (里面不能下拉了，并且方向向上，并且 头部漏出)
-            if (!ScrollingUtil.canChildScrollUp(target) && mIScroll.getScrollY() > 0 && mTopViewHeightOrg == mTopViewHeight) {
+        if (!ScrollingUtil.canChildScrollUp(target)) {
+            //不能 向上滚动既然 到头了
+            int hei=-1;
+            if(scrollOffset+dy<-maxScrollOffset){//因为向上滚动是负数
+                //这里不能全部消耗
+                hei=0;
+                consumed[1] = -maxScrollOffset-scrollOffset;
+            }else if(scrollOffset+dy>0){
+                //向下滚动的时候 头部不能大于0
+                hei=1;
+                consumed[1] =-scrollOffset;
+            } else{
+                hei=2;
+                //没到最大距离消耗全部
                 consumed[1] = dy;
-                Log.e(TAG, "scrollto_ 显示头部_下拉:" + dy);
-                mIScroll.scrollBy(0, dy);
-//            scrollBy(0, dy);
             }
-            if (!ScrollingUtil.canChildScrollUp(target) &&  mIScroll.getScrollY() <= 0) {
-                consumed[1] = dy;
-                Log.e(TAG, "scrollto_越过头部_下拉:" + dy);
-                mIScroll.scrollBy(0, dy);//仅仅记录 未消耗
-            }
-
-        } else if (dy>0){//上拉
-
-            if (getScrollY() < getStopHeight()) {
-                if (mTopViewHeightOrg == mTopViewHeight) {
-                    int length = mTopViewHeight - stopView.getMeasuredHeight() - getScrollY();
-                    consumed[1] = dy < length ? dy : length;
-                    Log.e(TAG, "scrollto_隐藏头部_上啦:" + dy);
-                    mIScroll.scrollBy(0, consumed[1]);
-                } else {//上啦 隐藏头部  和 布局调整的时候弄的
-//                    consumed[1] = dy;//吃掉 但
-//                    Log.e(TAG, "scrollto_头部变大_上啦:" + dy);
-//                    mIScroll.scrollBy(0, dy);
-                }
-            }
-
-            if (!ScrollingUtil.canChildScrollDown(target) && getScrollY() >= getStopHeight()) {
-                Log.e(TAG, "scrollto_越过头部_上啦:" + dy);
-                mIScroll.scrollBy(0, dy);//仅仅记录 未消耗
-            }
-
+            scrollOffset +=  consumed[1] ;
+            Log.e(TAG, "scrollto_ 显示头部_下拉:" + dy+" \t 消耗:"+consumed[1]+"\t 步骤:"+hei);
+            mIScroll.scrollBy(0, dy);
         }
-
-        System.out.println("scrollto_ total:" + mIScroll.getScrollY());
 
     }
 
@@ -139,31 +120,11 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
     }
 
 
-    @Override
-    public void scrollTo(int x, int y) {
-        System.out.println("scrollto x:" + x + "\t y:" + y);
-        //防止滑动出界； 滑动多了也不怕
-        if (y < 0)
-            y = 0;
-        if (y > getStopHeight())
-            y = getStopHeight();
-        super.scrollTo(x, y);
-    }
-
-    private int getStopHeight() {
-        return mTopViewHeight - stopView.getMeasuredHeight();
-    }
-
     private void upEvent() {
         //回弹
-        if (mIScroll.getScrollY() < 0) {
+        if (scrollOffset < 0) {
             Log.e(TAG, "scrollto_回弹->0！");
             mIScroll.smoothScrollTo_(0);
-        }
-
-        if (mIScroll.getScrollY() > getStopHeight()) {
-            Log.e(TAG, "scrollto_回弹->stopHeight！");
-            mIScroll.smoothScrollTo_(getStopHeight());
         }
 
     }
@@ -171,9 +132,8 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
 
     private View topView;
     private View contentView;
-    private int mTopViewHeight;
-    private View stopView;
-
+    private int maxScrollOffset = 300;
+    private int scrollOffset;
 
     @Override
     protected void onFinishInflate() {
@@ -189,10 +149,6 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
         }
     }
 
-    public void setEnterAlways(View v) {
-        stopView = v;
-    }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         //不限制顶部的高度
@@ -200,79 +156,17 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
         if (topView != null) {
             topView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
             System.out.println("顶部高度：" + topView.getMeasuredHeight());
-            ViewGroup.LayoutParams params = contentView.getLayoutParams();
-//            stopView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            params.height = getMeasuredHeight() - stopView.getMeasuredHeight();
             //设置要的高度 正好是 top的高度 加上 原来本身的高度；
-            setMeasuredDimension(getMeasuredWidth(), topView.getMeasuredHeight() + getMeasuredHeight());
+            setMeasuredDimension(getMeasuredWidth(), topView.getMeasuredHeight() + contentView.getMeasuredHeight());
         } else
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mTopViewHeight = topView.getMeasuredHeight();
-    }
-
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        //拦截 则不会走  nest嵌套
-        Log.i(TAG, "onInterceptTouchEvent");
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mTopViewHeightOrg = topView.getHeight();
-                Log.i(TAG, "onInterceptTouchEvent ACTION_DOWN");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.i(TAG, "onInterceptTouchEvent ACTION_MOVE");
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.i(TAG, "onInterceptTouchEvent ACTION_UP");
-                break;
-
-            default:
-                break;
-        }
-        return super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        Log.i(TAG, "onTouchEvent");
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                Log.i(TAG, "onTouchEvent ACTION_DOWN");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.i(TAG, "onTouchEvent ACTION_MOVE");
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.i(TAG, "onTouchEvent ACTION_UP");
-                break;
-            default:
-                break;
-        }
-        return super.onTouchEvent(ev);
-    }
-
-    @Override
-    public boolean canScrollVertically(int direction) {
-        if (direction < 0) {
-            return getScrollY() > 0;
-        } else {
-            return getScrollY() < mTopViewHeight;
-        }
-//        return super.canScrollVertically(direction);
     }
 
 
     //------------------------------------------- 滚动策略 ------------------------------------------------
     public abstract class IScrollFlex {
 
-        protected int scrollY;
         private static final int DEFAULT_DURATION = 250;
 
         private ValueAnimatorProxy valueAnimator = ValueAnimatorProxy.ofInt(0, 100)
@@ -290,7 +184,9 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
                     @Override
                     public void onAnimationUpdate(ValueAnimator animation) {
                         Integer offset = (Integer) animation.getAnimatedValue();
-                        scrollToCenter(offset, true);
+                        scrollOffset=-offset;
+//                        System.out.println("ZNestParentLayout addUpdateListener  offset==>"+offset);
+                        scrollTo_(offset, true);
                     }
 
                 });
@@ -304,29 +200,19 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
         }
 
         private void smoothScrollTo_(int fy) {
-            valueAnimator.setIntValues(getScrollY(), fy);
+            valueAnimator.setIntValues(-scrollOffset, fy);
             valueAnimator.start();
         }
 
-        private void scrollTo_(int fy) {
-            scrollToCenter(fy, false);
-        }
 
-        private void scrollToCenter(int fy, boolean isAnimate) {
-            scrollY = fy;
+        private void scrollTo_(int fy, boolean isAnimate) {
             scrollTo(fy, false);
             scrollCallback();
         }
 
         public void scrollCallback() {
             if (mIFlexScroll != null) {
-                int offsetMyself = 0, offsetOver = 0;
-                if (getScrollY() < getStopHeight() && getScrollY() > 0)
-                    offsetMyself = getScrollY();
-                else
-                    offsetOver = getScrollY();
-                Log.e(TAG, "scrollto_什么鬼:" + getScrollY());
-                mIFlexScroll.scroll(offsetMyself, offsetOver);
+                mIFlexScroll.scroll(-scrollOffset);
             }
         }
 
@@ -338,31 +224,28 @@ public class ZNestParentLayout extends LinearLayout implements NestedScrollingPa
 
         protected abstract void scrollBy(int fx, int fy);
 
-        private int getScrollY() {
-            return scrollY;
-        }
     }
 
     //弹性
     public class FlexScroll extends IScrollFlex {
 
         protected void scrollTo(int fy, boolean isAnimate) {
-            ZNestParentLayout.this.scrollTo(0, fy);
+//            ZNestParentLayout.this.scrollTo(0, fy);
         }
 
         @Override
         protected void scrollBy(int fx, int fy) {
             if (mIScroll.isRunning())
                 mIScroll.cancel();
-            scrollY += fy;
-            ZNestParentLayout.this.scrollBy(0, fy);
+//            scrollY += fy;
+//            ZNestParentLayout.this.scrollBy(0, fy);
             scrollCallback();
         }
     }
 
 
     public interface IFlexScroll {
-        void scroll(int offsetMyself, int offsetOver);
+        void scroll(int offset);
     }
 
     public IFlexScroll getIFlexScroll() {
